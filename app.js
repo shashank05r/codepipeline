58d2f5ab5cdc3bcafb4f70307333e93df23637c8
shashank05r <p.shashank05r@gmail.com>
2025-07-10 12:35:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+// app.js
 const http = require('http');
 const port = 80;
+
 const server = http.createServer((req, res) => {
   res.end("Hello from ECS Fargate!\n");
 });
-server.listen(port);
+
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
+});
